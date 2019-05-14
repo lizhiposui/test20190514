a9316f739971f99168195864b3eda81332405e0c
niezhong <niezhong@DESKTOP-SRNHFLE>
2019-05-14 18:54:11 +0800

    2019-05-14上传实体类

---
+++ src/cn/kgc/tangcco/niezhong/Person2.java
@@ -0,0 +1,15 @@
+package cn.kgc.tangcco.niezhong;
+
+public class Person2 {
+
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
